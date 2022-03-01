aa9fbdedcf7db045eb251c540720b0490381930d
hunteradv <gustavopaulody@gmail.com>
2022-03-01 17:05:45 -0300

    Incluindo projeto LinqToEntities (sql)

---
+++ AluraTunes.LinqToEntities/Program.cs
@@ -0,0 +1,28 @@
+﻿using AluraTunes.LinqToEntities.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluraTunes.LinqToEntities
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var context = new AluraTunesEntities())
+            {
+                var query = from g in context.Generoes
+                select g;
+
+                foreach (var genre in query)
+                {
+                    Console.WriteLine("{0}\t{1}", genre.GeneroId, genre.Nome);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AluraTunes.LinqToSQL/Program.cs
@@ -0,0 +1,24 @@
+﻿using AluraTunes.LinqToEntities.Data;
+using System;
+
+namespace AluraTunes.LinqToEntities
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (AluraTunesEntities context = new AluraTunesEntities())
+            {
+                var query = from g in context.Generoes
+                            select g;
+
+                foreach (var genre in query)
+                {
+                    Console.WriteLine("{0}\t{1}", genre.GeneroId, genre.genero.Nome);
+                }
+
+                //imprimir no console
+            }
+        }
+    }
+}
