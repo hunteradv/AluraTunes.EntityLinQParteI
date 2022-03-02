a066889c281a563042af780dae554ddac60d6159
hunteradv <gustavopaulody@gmail.com>
2022-03-01 22:12:24 -0300

    Adicionando duas consultas.

--- AluraTunes.LinqToEntities/Program.cs
+++ AluraTunes.LinqToEntities/Program.cs
@@ -13,12 +13,31 @@ namespace AluraTunes.LinqToEntities
         {
             using(var context = new AluraTunesEntities())
             {
-                var query = from g in context.Generoes
+                var query = from g in context.Generos
                 select g;
+
+                Console.WriteLine("Primeira query");
+                Console.WriteLine();
 
                 foreach (var genre in query)
                 {
                     Console.WriteLine("{0}\t{1}", genre.GeneroId, genre.Nome);
+                }
+
+                var query2 = from g in context.Generos
+                             join f in context.Faixas
+                             on g.GeneroId equals f.GeneroId
+                             select new { f, g };
+
+                query2 = query2.Take(10);
+
+                Console.WriteLine();
+                Console.WriteLine("Segunda query");
+                Console.WriteLine();
+
+                foreach (var item in query2)
+                {
+                    Console.WriteLine($"{item.f.Nome} \t {item.g.Nome}");
                 }
             }
 
