2c9aa030e2970c6e2d78327bab43c9f46934c8d5
hunteradv <gustavopaulody@gmail.com>
2022-03-07 14:12:04 -0300

    Adicionando um novo projeto na solution para consultas com count, sum e groupby.

---
+++ AluraTunes.CountSumGroupby/Program.cs
@@ -0,0 +1,17 @@
+﻿using AluraTunes.LinqToEntities.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluraTunes.CountSumGroupby
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new AluraTunesEntities())
+        }
+    }
+}
---
+++ AluraTunes.LinqToEntitiesCountSumGroupby/Program.cs
@@ -0,0 +1,35 @@
+﻿using AluraTunes.LinqToEntitiesCountSumGroupby.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluraTunes.LinqToEntitiesCountSumGroupby
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new AluraTunesEntities())
+            {
+                var query = from f in context.Faixas
+                            where f.Album.Artista.Nome == "Led Zeppelin"
+                            select f;
+
+                //Método 1
+                //var quantity = query.Count();
+
+                //Console.WriteLine($"Led Zeppelin tem {quantity} músicas no banco de dados");
+
+
+                //Método 2
+                var quantity = context.Faixas.Count(f => f.Album.Artista.Nome == "Led Zeppelin");
+
+                Console.WriteLine($"Led Zeppelin tem {quantity} músicas no banco de dados");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
