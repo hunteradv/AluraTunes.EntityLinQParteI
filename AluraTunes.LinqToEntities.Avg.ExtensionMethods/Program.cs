6160335bd7d90e1520385c34b7e84d5efc6cbc1a
hunteradv <gustavopaulody@gmail.com>
2022-03-08 22:33:34 -0300

    Incluindo novo projeto na solution.

---
+++ AluraTunes.LinqToEntities.Avg.ExtensionMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluraTunes.LinqToEntities.Avg.ExtensionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
