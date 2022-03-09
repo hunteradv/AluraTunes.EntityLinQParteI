c25a042ee8cca18766782f2089fee9dfeabd7ccf
hunteradv <gustavopaulody@gmail.com>
2022-03-09 17:56:27 -0300

    Criando consulta para encontrar venda média e mediana.

--- AluraTunes.LinqToEntities.Avg.ExtensionMethods/Program.cs
+++ AluraTunes.LinqToEntities.Avg.ExtensionMethods/Program.cs
@@ -13,11 +13,17 @@ namespace AluraTunes.LinqToEntities.Avg.ExtensionMethods
         {
             using (var context = new AluraTunesEntities())
             {
-                context.Database.Log = Console.WriteLine;
+                //MIN MAX E AVG
+
+                //context.Database.Log = Console.WriteLine;
 
                 var biggestSale = context.NotasFiscais.Max(nf => nf.Total);
                 var lowestSale = context.NotasFiscais.Min(nf => nf.Total);
                 var averageSale = context.NotasFiscais.Average(nf => nf.Total);
+
+                Console.WriteLine();
+                Console.WriteLine("Método 1");
+                Console.WriteLine();
 
                 Console.WriteLine($"A maior venda é de ${biggestSale}");
                 Console.WriteLine($"A menor venda é de ${lowestSale}");
@@ -32,9 +38,37 @@ namespace AluraTunes.LinqToEntities.Avg.ExtensionMethods
                                 averageSale = grouped.Average(nf => nf.Total)
                             }).Single();
 
+                Console.WriteLine();
+                Console.WriteLine("Método 2");
+                Console.WriteLine();
+
                 Console.WriteLine($"A maior venda é de $ {sales.biggestSale}");
                 Console.WriteLine($"A menor venda é de $ {sales.lowestSale}");
                 Console.WriteLine($"A venda média é de $ {sales.averageSale}");
+
+
+                //MEDIANA  
+
+                Console.WriteLine();
+                Console.WriteLine("MEDIANA");
+                Console.WriteLine();
+
+
+                var averageSales = context.NotasFiscais.Average(invoice => invoice.Total);
+
+                Console.WriteLine($"Venda média: {averageSale}");
+
+                var query = from invoice in context.NotasFiscais
+                            select invoice.Total;
+
+                var count = query.Count();
+                var ordenedQuery = query.OrderBy(invoice => invoice);
+
+                var centralElement = ordenedQuery.Skip(count / 2).First();
+
+                var median = centralElement;
+
+                Console.WriteLine($"Mediana: {median}");
             }
 
             Console.ReadKey();
